8e0d578b4f483766a497c5b5ed471ee5413f5b1b
rekuskz-gif <rekuskz@gmail.com>
2026-05-09 22:34:00 +0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,4 @@
 const admin = require('firebase-admin');
-
-// Инициализируем Firebase ОДИН РАЗ
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
@@ -10,10 +8,6 @@ if (!admin.apps.length) {
     }),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
-  
   console.log('✅ Firebase инициализирован');
-} else {
-  console.log('✅ Firebase уже инициализирован');
 }
-
 module.exports = admin;
