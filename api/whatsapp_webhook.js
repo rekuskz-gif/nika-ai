a0bd0710d19ebe1ca42e3e93cd23ae362a55ed61
rekuskz-gif <rekuskz@gmail.com>
2026-05-09 13:19:59 +0300

    Create whatsapp_webhook.js

---
+++ api/whatsapp_webhook.js
@@ -0,0 +1,6 @@
+// Получает сообщения от Green-API (WhatsApp)
+// Ищет клиента в Google Sheets
+// Сохраняет в Firebase
+// Отправляет в Claude
+// Отправляет ответ в WhatsApp
+// Уведомляет менеджера в Telegram
