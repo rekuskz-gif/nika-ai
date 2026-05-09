f1550ec0f9b08ab228dbbda0cdd101367808bcee
rekuskz-gif <rekuskz@gmail.com>
2026-05-09 13:27:45 +0300

    Delete api/authentication.js

--- api/authentication.js
+++
@@ -1,5 +0,0 @@
-// Добавить:
-// - Получение channel из req.body ("website" или "whatsapp")
-// - Выбор правильного google DocId в зависимости от channel
-// - Логику отправки ответа в WhatsApp (если channel === "whatsapp")
-// - Обновление spent tokens в Google Sheets
