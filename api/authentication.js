f6b6cbc9d92697188f3f5d2f081660de8d02d509
rekuskz-gif <rekuskz@gmail.com>
2026-05-09 13:20:34 +0300

    Create authentication.js

---
+++ api/authentication.js
@@ -0,0 +1,5 @@
+// Добавить:
+// - Получение channel из req.body ("website" или "whatsapp")
+// - Выбор правильного google DocId в зависимости от channel
+// - Логику отправки ответа в WhatsApp (если channel === "whatsapp")
+// - Обновление spent tokens в Google Sheets
